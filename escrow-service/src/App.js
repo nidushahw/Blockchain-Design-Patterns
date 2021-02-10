9cc4d39fa5108c6dff7545fe5192f04141870b1f
Nidusha Lakmali <nidushahw@yahoo.com>
2021-02-09 21:24:53 -0500

    update app.js

--- escrow-service/src/App.js
+++ escrow-service/src/App.js
@@ -16,18 +16,6 @@ class App extends React.Component {
 
   componentDidMount = async () => {
     await EscrowService.init();
-    let accounts = EscrowService.getAccounts();
-
-    // const fee = await EscrowService.getEscrowFee();
-    // console.log("escfserefee>>", fee);
-    console.log(accounts[1], accounts[2]);
-    // let tx = await EscrowService.addEscrow("0x0e35AaAcf8884B2eBf271B5A60b68adbb7c53780", "0xeeBF3ef1FB4421752C7803A96774ba70e0b2f975", "0.001");
-    //  console.log('last transaction>>>>>', JSON.stringify(tx));
-
-    let count = await EscrowService.getEscrowCount();
-    console.log(accounts[0], ">>>>>>>>>>>>>>>>", count);
-    // let escrow = await EscrowService.getEscrowByTrnxId(accounts[0], count - 1);
-    // console.log(escrow, JSON.stringify(escrow));
   }
 
   render() {
